5d3fade7d13a0f11721aa7905a4036e94378d743
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2021-04-08 22:35:35 -0500

    this implementation clearly more performant

--- 1.7.scm
+++ 1.7.scm
@@ -35,3 +35,28 @@
 ;(sqrt 1e+58), spins out on 1.00..0001e29, not enought zeos left to get there
 ;bam, here it is, stack overflow because we lose the precision for our guess to ever be good enough
 ;FAILS by ANSWERING TOO SLOW (never)
+
+
+;trying to add another watch variable
+(define (sqrtImproved goal)
+(define (good-enough?2 guess last goal)
+  (printf "~a ~a ~a\n" guess last  (abs (- guess last)))
+  (<= (abs (- guess last)) (/ guess 100)))
+  ; (< (abs (- (square guess) goal)) (* 0.001 ))
+(define (improve2 guess goal) (average guess (/ goal guess)))
+(define (sqrt-iter2 guess last goal)
+    (printf "~a ~a ~a\n" guess last goal)
+    (if (good-enough?2 guess last goal)
+        guess
+        (sqrt-iter2 (improve2 guess goal) guess goal)))
+  (sqrt-iter2 1.0 0.1 goal))
+
+ (sqrtImproved 25)
+(print "small num tests\n")
+(sqrtImproved 0.0001)
+(sqrtImproved 0.0000001)
+(sqrtImproved 0.00000000000000000000000001)
+;better!
+(print "big num tests\n")
+(sqrtImproved 1e+58);bottomed out this time
+(sqrtImproved 1e+100)
